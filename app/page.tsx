eff61e4068cf677a3c1542650f86be700592afd7
ManshulDuggal <d-manshul@ankh-systems.co.jp>
2024-08-01 23:22:24 +0900

    final changes

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,26 @@
+import Description from "@/components/custom/Description";
+import Options from "@/components/custom/Options";
+import { Slide } from "@/components/custom/Slide";
+import React from "react";
+
+interface Props {}
+
+//bg-gradient-to-br from-stone-500  bg-background
+
 import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24"></main>
+    <main className="flex min-h-screen flex-col items-center justify-between">
+      <section className="p-5  lg:flex lg:flex-col lg:gap-20 gap-10 w-screen justify-center justify-items-center bg-[url('/bg.jpg')] h-screen ">
+        <section className="lg:flex lg:gap-10 lg:justify-around lg:items-center max-w-5xl m-auto bg-stone-500 shadow-md px-20 py-2 rounded-md">
+          <Description />
+          <Slide />
+        </section>
+        <section>
+          <Options />
+        </section>
+      </section>
+    </main>
   );
 }
